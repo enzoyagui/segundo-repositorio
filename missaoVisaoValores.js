661f8add679b61e1b4a23c17a7d59aaffdce76bf
enzoyagui <143002331+enzoyagui@users.noreply.github.com>
2024-11-06 22:06:06 -0300

    06/11

--- missaoVisaoValores.js
+++ missaoVisaoValores.js
@@ -1,16 +1,16 @@
 criaCartao (
     'Missão',
     'Saiba qual é a missão de nossa empresa!',
-    'Fornecer paçocas de qualidade excepcional para atender às necessidades de nossos clientes de forma eficiente e ética.'
+    ''
 
 criaCartao (
     'Visão',
     'Saiba qual é a visão de nossa empresa!',
-    'uma forma fácil e prática de começar empreende e ganhar seu propio dinheiro '
+    ''
 )
 
 criaCartao (
     'Valores',
     'Saiba quais são os valores de nossa empresa!',
-    'qualidade sempre produtos de qualidade e saborosos Inovação jeitos diferentes de vender paçocas independência como sou um vendedor ambulante não dependo de funcionários para fazer acontecer '
+    ''
 )
