716446c6063da3278c873b432316362671eb5d8d
enzoyagui <143002331+enzoyagui@users.noreply.github.com>
2025-04-07 22:49:20 -0300

    salve

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentarfonte');
+    aumentaFonteBotao.addEventListener('click', function(){ 
+
+        })
+        const botaoDeAcessibilidade = document.getElementById
+        ('botao-acessibilidade');
+        const opcoesDeAcessibilidade = document.getElementById
+        ('opcoes-acessibilidade');
+        
+    })
+
+
