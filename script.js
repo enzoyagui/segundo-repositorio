ad5dbc2a7b42c5f1eb43449b9de4508ab27f4da4
enzoyagui <143002331+enzoyagui@users.noreply.github.com>
2025-05-22 22:30:58 -0300

    22/5

--- script.js
+++ script.js
@@ -16,10 +16,13 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
-        document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     })
+    alternaContraste.addEventListener('click', function(){
+         document.body.classList.toggle('alto-contraste')
+ })
 })
